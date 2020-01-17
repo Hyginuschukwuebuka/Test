6e2277df92f4b99f285b36cb0139d843d55a5a5f
HyginusChukwuebuka <hyginuschukwuebuka1234@gmail.com>
2020-01-17 16:26:14 +0100

    adding starting project

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public String firstName;
+        public String lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + firstName + " " + lastName);
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("buka!");
+            var john = new Person();
+            john.firstName = "Derichman";
+            john.lastName = "Eze";
+            john.Introduce();
+        }
+    }
+}
